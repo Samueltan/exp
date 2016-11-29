5e76df35b1593ab0cd5f6b4d770ebf45373c1d25
samueltan <samuel.tan@sophos.com>
2016-11-29 17:01:09 -0500

    composite - transparency first

--- src/exp/dp/composite/AbstractFile.java
+++ src/exp/dp/composite/AbstractFile.java
@@ -2,8 +2,18 @@ package exp.dp.composite;
 
 //抽象文件类：抽象构件
 abstract class AbstractFile {
-    public abstract void add(AbstractFile file);
-    public abstract void remove(AbstractFile file);
-    public abstract AbstractFile getChild(int i);
+    public void add(final AbstractFile file) {
+        System.out.println("对不起，不支持该方法！");
+    }
+
+    public void remove(final AbstractFile file) {
+        System.out.println("对不起，不支持该方法！");
+    }
+
+    public AbstractFile getChild(final int i) {
+        System.out.println("对不起，不支持该方法！");
+        return null;
+    }
+
     public abstract void killVirus();
 }
